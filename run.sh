95654bc9cd751dec7d9852079fb43591bff0d8aa
JeremyOT <jeremyot@gmail.com>
2014-08-02 12:37:46 -0700

    fix arg passthrough

--- run.sh
+++ run.sh
@@ -2,14 +2,14 @@
 
 case $1 in
   nodetool)
-    /usr/lib/cassandra/bin/nodetool ${@:2}
+    /usr/lib/cassandra/bin/nodetool "${@:2}"
     ;;
   cqlsh)
-    /usr/lib/cassandra/bin/cqlsh ${@:2}
+    /usr/lib/cassandra/bin/cqlsh "${@:2}"
     ;;
   *)
     cp -r /usr/lib/cassandra/conf/* /var/cassandra/config
-    /var/cassandra/config.py /usr/lib/cassandra/conf /var/cassandra/config ${@:2}
+    /var/cassandra/config.py /usr/lib/cassandra/conf /var/cassandra/config "${@:2}"
     CASSANDRA_CONF=/var/cassandra/config /usr/lib/cassandra/bin/cassandra -f
     ;;
 esac
